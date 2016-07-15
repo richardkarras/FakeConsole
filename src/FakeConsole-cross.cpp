14b6a43391979504e8068589e783632d9c68da47
richardkarras <richard.karras@gmail.com>
2016-07-15 17:14:17 -0600

    with hint added

--- src/FakeConsole-cross.cpp
+++ src/FakeConsole-cross.cpp
@@ -31,31 +31,31 @@ int main()
 		}
 		
 		//ASCII output of breakout logo
-		cout << "\n vivvvrvvvrvrvrvrvrvrvrvrvvvrvrvrvrvvvrvrvrvrvrvrvrvrvrvrvrvrvrvrvvvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvvvvvrvrvrvrvrvvvvvrvvvvvvvrvrvrvrvvvr";
-		cout << "\n ULJjYjJJYjJjYjYjJjYuYjYjYuYjYjJjYuYuYjJjYjYjYjYjJjJuJjYuJjYJJuYjJJJuYjYuJjJjYjYjJjJJYJLjJuJjJJYJLJYuJuYjYJYjYjYuJjJuYjYuJjYuYjYuJuJuYjJJYjJjJJJjYjYjYY";
-		cout << "\n UYUuouUuouUuUuUuUuouUuUuouUuUuUuUuououououououUuUuUuouUuUuUuUuUuouououUuououououUuUuujjjoVNkkoouUuouUuUuouUuUuUuouUuUuouUuUuouUuUuUuUuUuUuUuououUuUuoj";
-		cout << "\n jJuuuuuuuujujuuUjujuuujujuuUjuuujuuuuuuUjujujujUuUuujujUuUjuuujujuuujUjUjujuuujujuLLvLJuoIruXZFouuuuuujuuuuUjUjuuujuuujUjujujuuuuujuuujujuuujUjUjUjUjj";
-		cout << "\n uLUjuuuuuJujujujuJujujujujuuujujujujuuujujujuuuuujuJujujuuuuujujujuJujuuuJuJujujuvvIOBBuS    ikXouujujujujujujuJujuJuJujujuuujuJuuuuujujujujujujujuuuY";
-		cout << "\n jJjujujuJujujujujujuJuJujujujuuuJujujujuJujuuujujujujujuuuuujujuJuJujujuJujujuuuvjMBMBXJJ      ISojujujuJujuuUjujuJujujujujuuuuUjuuUjujuuujUuujuuujujj";
-		cout << "\n uLuJJLYLYLJYuJuJuJuLYLYLJYJJuJujujJLJLYLYLJJuJuJuYYLJYujujujjLYLjJuJjLYLjJuuujuLYMBBZLLJokGEi   quuJuuuuUuoIkFSIIuUjujUuoUIUUjUuoUoUuuUUIoVoIoIUouUjuJ";
-		cout << "\n jYJjYYLYLLvLvLJujuLjLYLYLLvvvYJuJJYJLLLYLJJjJujuLJYYYYYujujuYjLYYJYYvJLYjUJujujLjBBBvvJuuooGj   PSuujujUoNNqSSFqNNIojuUXPNqXoUUFXNNqIoIPqENZNZNEkkuUjj";
-		cout << "\n ULjJBBBBBBBMGuLLuLuMBBBBBBBGSLLJYUBMBMBBBBBjJJuYYMBBBXvJujuvXBBBBjvvBMBBBUjjuJYrjBBMvvJjUoPOI   MNXuujoSNr       rNFUUXi    FIkr    Jqv          iXuuY";
-		cout << "\n uJLuBBBBMMBBMBXLYYLBBBBBBBBBBBJLvoMBBBBBBBBuYuJLuBBBBBvJjujLkBBBBJrNBBBBVLjuYjYvLBBMvvLjUJvVv   VuvVUuVP           PFVN     kFPr    YOi           Pujj";
-		cout << "\n uLJYBBBBUijBBBBJYLYMBMBFrLBBBBOvLuBBBBorLvYJuJjvNBBMBBUvujuvkBBBBvuBBBBVvYujLSBBBBBBMBBjVv         uVuNi    uZu    iNoN     qFNr    uqovIL    iVLJVuuY";
-		cout << "\n uLLJBBMBUirMMBBILYLBBBBSrrqBMBBYvuMBMBUvLJJuJuLLMBMBMBNvYuJLFBMBMvOBBBPvYuuuLkMBBBMBBBMBqOi        VoFX    iMNMi    kXN     Xkqv    JqoPGX    vBPFuUjj";
-		cout << "\n ULYLBBBBIiVBBBOvJLJBBMBIirOBBMOvvuBBBBUivLuJuJLoBBMXBBBvJjuvSBBMMEBMBqvLjJuJjvviNBMMBBMFv       uBGFIVk    iZVZi    kkE     qFNr    uSUuXu    rNojuuuL";
-		cout << "\n jLLJBBMBBBBBGUrLYYLBBBMOPMBBBBjLvuBBMBOOBELujjvGBBGYBBBVvjJLSBMMMBMBMYrjjuJuLVMOMBMBBovv           oIFk    iNVNi    kPN     XSPv    jPuuSo    iEuuuujj";
-		cout << "\n uLJYBMBBMMBMMEuvjLYBBMBMBMBMkvvJLuBBMMBBBMLJuvjMBBNrBMBZvJjvkMBMBBBMBGvLuuujYIBOBBMMBNvSr        iioVVX    iNIZi    XkZ     qVNr    ukUjXu    rqUjujuY";
-		cout << "\n jYLjBBMBuiYBBBBFvYLBMBBoSBBBLrYuvUBBMBILuuYuJvkBBBuiEBBBLLYLSBMMMXZBBBFvYujuJLrrqBMBBBPUP       rOFSuSX    iNFNi    SPN     Pkqv    jPuukU    iEuuuuJj";
-		cout << "\n ULYYBBBMSrrPBBBOLLYBBMBULMBBMvJjLjBBBBIrLYujJvMBBBNIMBBMXvjvkBBBBruMBBBLLjuJLSBBBMBMBMjLi          UVIX    iEIZi    kSE     NFNr    uSUuXu    rNojuuuJ";
-		cout << "\n uYLJMBBBVrrNBBMMvLLBBMBkiMBBMSvjvuMBMBUrLYYjvuBBMBBBBBMBOLLvFBMBMLrOMBMGvjjuLUqkOBMMMBMBMMNr    iFYFuFk    iGXOi    SSNi    NNZv    ukuUSo    iNuujujj";
-		cout << "\n ULYYBBBBVrUMBMBXLLJBBMBSrIBBBMLvLuBBBBorLLJLvPBMBGYLoBBMBjLvSBBBBLrjBBBMFvujjLYvZBBMMBBBMji     iJLVIUN     GMZ     NUNr    XMBi    qouuPu    rqojujuL";
-		cout << "\n uLLuMBBBBBBBBBGYLYYBBBBqrLMBBBZvvIBBBBMBBBBJvBBBBXrvvBBBBGvvXBBBBuvvEBBBBJLuvXBBBBBBBBOvo          oIoFI           VSUVP           vXUuuSu    iNuujuJJ";
-		cout << "\n uLjjBBBMBBBOZUvLjLuBBMBkLvNBBBBuLuBBMMBMBMBLVBBBBuLYvPBMBMJvXBBBBuYvuMBMBNJJYVBMBMMMBBMLFr  i i    oVjoPF         FXUjoFPi        LNIuuukY    iXUjuuuJ";
-		cout << "\n uYjJLLvLvLvLvLLuJuLLvLLJJjLLvLLjYjLLvvvLvLLJLJvLvJJjJLvLvjJjLLvLLJjjLLvLLuYuJJLLvLvLvLLjuSNENZNZNEXIuujoSZSjvvvuSZSojuuoFZkuvvrLoEXIuujuuVuVoooojuuujJ";
-		cout << "\n uLujjYjYjLjJjJujujuJjYjJujuYjYjJuJjLjLjYjYjJuYjYuJujuJjYjYujuYjYjJujuJJYuYujuJJYjLJYJLujuuUUouoUouUuuuuuouSXNPNkSUojujuuuUFkNPNXkoouujUjUUSSkVIuUjujuY";
-		cout << "\n uYuUuujUjUuUuUuUuUuuuujUuuuUjUuUjUjUjuuUjUjUuuuuuUjUjuuuuUjUjUuUuUuUuujuuUuuuUuUuujUjujujUuuuUuUuUuUuUuUjUuUuouUuuuUuUjUuUuUuououUuujUuUuUuUuUuUjUjUju";
-		cout << "\n ovYLYLJLJLJLYLJLJLYLJLJLJLYLJLJLYLJLYLYLJLJLJLYLYLYLYLJLYLJLYLYLYLJLJLJLYLYLJLJLYLJLYLYLYLYLJLJLJLYLJLJLJLJLYLJLJLJLYLYLJLYLYLJLJLJLJLYLJLJLYLYLYLYYJv";
+		cout << "\n vivvvrvvvrvrvrvrvrvrvrvrvvvrvrvrvrvvvrvrvrvrvrvrvrvrvrvrvrvrvrvrvvvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvrvvvvvrvrvrvrvrvvvvvrvvvvvvvrvrvrvrvvvr" << endl;
+		cout << " ULJjYjJJYjJjYjYjJjYuYjYjYuYjYjJjYuYuYjJjYjYjYjYjJjJuJjYuJjYJJuYjJJJuYjYuJjJjYjYjJjJJYJLjJuJjJJYJLJYuJuYjYJYjYjYuJjJuYjYuJjYuYjYuJuJuYjJJYjJjJJJjYjYjYY" << endl;
+		cout << " UYUuouUuouUuUuUuUuouUuUuouUuUuUuUuououououououUuUuUuouUuUuUuUuUuouououUuououououUuUuujjjoVNkkoouUuouUuUuouUuUuUuouUuUuouUuUuouUuUuUuUuUuUuUuououUuUuoj" << endl;
+		cout << " jJuuuuuuuujujuuUjujuuujujuuUjuuujuuuuuuUjujujujUuUuujujUuUjuuujujuuujUjUjujuuujujuLLvLJuoIruXZFouuuuuujuuuuUjUjuuujuuujUjujujuuuuujuuujujuuujUjUjUjUjj" << endl;
+		cout << " uLUjuuuuuJujujujuJujujujujuuujujujujuuujujujuuuuujuJujujuuuuujujujuJujuuuJuJujujuvvIOBBuS    ikXouujujujujujujuJujuJuJujujuuujuJuuuuujujujujujujujuuuY" << endl;
+		cout << " jJjujujuJujujujujujuJuJujujujuuuJujujujuJujuuujujujujujuuuuujujuJuJujujuJujujuuuvjMBMBXJJ      ISojujujuJujuuUjujuJujujujujuuuuUjuuUjujuuujUuujuuujujj" << endl;
+		cout << " uLuJJLYLYLJYuJuJuJuLYLYLJYJJuJujujJLJLYLYLJJuJuJuYYLJYujujujjLYLjJuJjLYLjJuuujuLYMBBZLLJokGEi   quuJuuuuUuoIkFSIIuUjujUuoUIUUjUuoUoUuuUUIoVoIoIUouUjuJ" << endl;
+		cout << " jYJjYYLYLLvLvLJujuLjLYLYLLvvvYJuJJYJLLLYLJJjJujuLJYYYYYujujuYjLYYJYYvJLYjUJujujLjBBBvvJuuooGj   PSuujujUoNNqSSFqNNIojuUXPNqXoUUFXNNqIoIPqENZNZNEkkuUjj" << endl;
+		cout << " ULjJBBBBBBBMGuLLuLuMBBBBBBBGSLLJYUBMBMBBBBBjJJuYYMBBBXvJujuvXBBBBjvvBMBBBUjjuJYrjBBMvvJjUoPOI   MNXuujoSNr       rNFUUXi    FIkr    Jqv          iXuuY" << endl;
+		cout << " uJLuBBBBMMBBMBXLYYLBBBBBBBBBBBJLvoMBBBBBBBBuYuJLuBBBBBvJjujLkBBBBJrNBBBBVLjuYjYvLBBMvvLjUJvVv   VuvVUuVP           PFVN     kFPr    YOi           Pujj" << endl;
+		cout << " uLJYBBBBUijBBBBJYLYMBMBFrLBBBBOvLuBBBBorLvYJuJjvNBBMBBUvujuvkBBBBvuBBBBVvYujLSBBBBBBMBBjVv         uVuNi    uZu    iNoN     qFNr    uqovIL    iVLJVuuY" << endl;
+		cout << " uLLJBBMBUirMMBBILYLBBBBSrrqBMBBYvuMBMBUvLJJuJuLLMBMBMBNvYuJLFBMBMvOBBBPvYuuuLkMBBBMBBBMBqOi        VoFX    iMNMi    kXN     Xkqv    JqoPGX    vBPFuUjj" << endl;
+		cout << " ULYLBBBBIiVBBBOvJLJBBMBIirOBBMOvvuBBBBUivLuJuJLoBBMXBBBvJjuvSBBMMEBMBqvLjJuJjvviNBMMBBMFv       uBGFIVk    iZVZi    kkE     qFNr    uSUuXu    rNojuuuL" << endl;
+		cout << " jLLJBBMBBBBBGUrLYYLBBBMOPMBBBBjLvuBBMBOOBELujjvGBBGYBBBVvjJLSBMMMBMBMYrjjuJuLVMOMBMBBovv           oIFk    iNVNi    kPN     XSPv    jPuuSo    iEuuuujj" << endl;
+		cout << " uLJYBMBBMMBMMEuvjLYBBMBMBMBMkvvJLuBBMMBBBMLJuvjMBBNrBMBZvJjvkMBMBBBMBGvLuuujYIBOBBMMBNvSr        iioVVX    iNIZi    XkZ     qVNr    ukUjXu    rqUjujuY" << endl;
+		cout << " jYLjBBMBuiYBBBBFvYLBMBBoSBBBLrYuvUBBMBILuuYuJvkBBBuiEBBBLLYLSBMMMXZBBBFvYujuJLrrqBMBBBPUP       rOFSuSX    iNFNi    SPN     Pkqv    jPuukU    iEuuuuJj" << endl;
+		cout << " ULYYBBBMSrrPBBBOLLYBBMBULMBBMvJjLjBBBBIrLYujJvMBBBNIMBBMXvjvkBBBBruMBBBLLjuJLSBBBMBMBMjLi          UVIX    iEIZi    kSE     NFNr    uSUuXu    rNojuuuJ" << endl;
+		cout << " uYLJMBBBVrrNBBMMvLLBBMBkiMBBMSvjvuMBMBUrLYYjvuBBMBBBBBMBOLLvFBMBMLrOMBMGvjjuLUqkOBMMMBMBMMNr    iFYFuFk    iGXOi    SSNi    NNZv    ukuUSo    iNuujujj" << endl;
+		cout << " ULYYBBBBVrUMBMBXLLJBBMBSrIBBBMLvLuBBBBorLLJLvPBMBGYLoBBMBjLvSBBBBLrjBBBMFvujjLYvZBBMMBBBMji     iJLVIUN     GMZ     NUNr    XMBi    qouuPu    rqojujuL" << endl;
+		cout << " uLLuMBBBBBBBBBGYLYYBBBBqrLMBBBZvvIBBBBMBBBBJvBBBBXrvvBBBBGvvXBBBBuvvEBBBBJLuvXBBBBBBBBOvo          oIoFI           VSUVP           vXUuuSu    iNuujuJJ" << endl;
+		cout << " uLjjBBBMBBBOZUvLjLuBBMBkLvNBBBBuLuBBMMBMBMBLVBBBBuLYvPBMBMJvXBBBBuYvuMBMBNJJYVBMBMMMBBMLFr  i i    oVjoPF         FXUjoFPi        LNIuuukY    iXUjuuuJ" << endl;
+		cout << " uYjJLLvLvLvLvLLuJuLLvLLJJjLLvLLjYjLLvvvLvLLJLJvLvJJjJLvLvjJjLLvLLJjjLLvLLuYuJJLLvLvLvLLjuSNENZNZNEXIuujoSZSjvvvuSZSojuuoFZkuvvrLoEXIuujuuVuVoooojuuujJ" << endl;
+		cout << " uLujjYjYjLjJjJujujuJjYjJujuYjYjJuJjLjLjYjYjJuYjYuJujuJjYjYujuYjYjJujuJJYuYujuJJYjLJYJLujuuUUouoUouUuuuuuouSXNPNkSUojujuuuUFkNPNXkoouujUjUUSSkVIuUjujuY" << endl;
+		cout << " uYuUuujUjUuUuUuUuUuuuujUuuuUjUuUjUjUjuuUjUjUuuuuuUjUjuuuuUjUjUuUuUuUuujuuUuuuUuUuujUjujujUuuuUuUuUuUuUuUjUuUuouUuuuUuUjUuUuUuououUuujUuUuUuUuUuUjUjUju" << endl;
+		cout << " ovYLYLJLJLJLYLJLJLYLJLJLJLYLJLJLYLJLYLYLJLJLJLYLYLYLYLJLYLJLYLYLYLJLJLJLYLYLJLJLYLJLYLYLYLYLJLJLJLYLJLJLJLJLYLJLJLJLYLYLJLYLYLJLJLJLJLYLJLJLYLYLYLYYJv" << endl;
 		do
 		{
 			
@@ -102,15 +102,28 @@ void console()
         else if (con_input == "ls")
             cout << " \n Directory listing: \n secdoc.txt \n 1 file, 0 folders \n "; // Output "directory listing" to allow participants to attempt to open the photo clue.
 		else if (con_input == "write secdoc.txt")
+		{
 			graphic_display();
 			return;
+		}
     }
     return;
 }
 
 void graphic_display()
 {
-    cout << "output file";
-        
+
+	cout << "MEMORADNDUM FOR HQ USAF/WB \nOctober 30th 1981\n" << endl;
+	cout << "FROM: 	SECDEF\n		1600 Pennsylvania Avenue\n		Washington D.C. 20330 - 1250\n" << endl;
+	cout << "SUBJECT: Developmental plans of project E.S.C.R.M\n\n" << endl;
+	cout << "1. Project E.S.C.R.M or Electromagnetic Solar Control Recon Missile is currently being \nconstructed to help us end the current hostilities between the United States and the \nU.S.S.R. The weapon has been designed for deep space flight and will be the first \nsteps toward the Star Wars Defense system.\n" << endl;
+	cout << "2. The location of Project E.S.C.R.M is currently classified as Top Secret and only \nlevel 2 security clearance personnel are aware of its location. For security reasons \nthe location of Project E.S.C.R.M has been \nseparated into 4 documents which have been \nstored in Archive Room 2B, The records room is off limits to all personnel until \nfurther notice.\n" << endl;
+	cout << "3. According to recent assessments of project E.S.C.R.M we are well under our \nprojected time table. Preliminary reports suggest that BIG RED does not currently know \nof the existence of Project E.S.C.R.M and are preoccupied with the current Sweden \nincident.\n" << endl;
+	cout << "4. Currently Project E.S.C.R.M is being overseen by Stewart Littleton, one of the \nfinest minds in the world on rocketry and propulsion. He has set a very strict time \ntable and we have encountered some issues with supplying the required parts for him, \nwe hope to get back on schedule by mid month.\n" << endl;
+	cout << "5.  General Coulton has requested that he be contacted when Project E.S.C.R.M is at \n90% completion, he wishes to tour the facility to ensure everything is up to \nstandards. He can be reached at EXT 918 during normal business hours.\n" << endl;
+	cout << "6. The current time table sets the completion of the device on November 20th at 06:00 \nhours, however with current delays we may not see completion until next spring. All \nfunding has been diverted to Project E.S.C.R.M to help rectify this.\n" << endl;
+	cout << "	1A: Officers note, we have changed the control password on the weapons cage to \nthe following 06751 going forward the control password will change every month, new \npasswords will be distributed by courier to all authorized officers." << endl;
+	cout << "Report Complete \nSergeant Louis Benton \nUSAF - Wallaby Base" << endl;
+    
     return;
 }
