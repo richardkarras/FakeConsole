94dbefbbc666c6bcfbb95b20ca10b36492b31138
richardkarras <richard.karras@gmail.com>
2016-07-25 22:17:18 -0600

    Add setup scripts
    Add additional startup scripts to github

---
+++ Breakout.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Console Load script.  Runs key killer script and starts Breakout puzzle 
+echo "Setting up......."
+
+/root/keykill.sh
+
+echo "Launching...."
+
+/root/ConsolePuzzle
+
---
+++ keykill.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Key Kill script
+# WARNING this will disable all control keys, such as alt, control.  Use at own risk
+echo "Disabling keys"
+loadkeys nohotkey.map
+echo "Keys killed"
