60b4f72c2612a05726b34e17adab2285bed0df02
richardkarras <richard.karras@gmail.com>
2016-11-24 18:00:32 -0600

    updated weapondoc to weapon

--- src/FakeConsole-cross.cpp
+++ src/FakeConsole-cross.cpp
@@ -98,24 +98,12 @@ void console()
 		if (con_input == "help")
 			cout << "\n type dir or ls for directory listing \ntype file name to print text to screen \n for options on commands type 'help <command>'\n"; // Leading info for users to work with unfamiliar commands.
 		else if (con_input == "ls"||con_input == "dir")
-			cout << "\n\n Directory listing: \n weapon.txt \n 1 file, 0 folders \n "; // Output "directory listing" to allow participants to attempt to open the photo clue.
-		else if (con_input == "weapondoc.txt"||con_input=="weapondoc")
+			cout << "\n\n Directory listing: \n secret.txt \n weapon.txt \n 2 files, 0 folders \n "; // Output "directory listing" to allow participants to attempt to open the photo clue.
+		else if (con_input == "weapon.txt"||con_input=="weapon")
 			graphic_display();
 		else if (con_input == "secret.txt"||con_input == "secret")
 		{
 			morse_code();
-		}
-		else if (con_input == "ls -a" || con_input == "dir /a")
-		{
-			cout << "\n\n Directory listing: \n secret.txt \n weapondoc.txt \n 2 files, 1 hidden, 0 folders \n "; // Output "directory listing" to allow participants to attempt to open the photo clue and morse code clue.
-		}
-		else if (con_input == "help ls"|| con_input == "man ls")
-		{
-			cout << "Displays a list of files and subdirectories in a directory. \n ls [options] \n\n -a Show all files, including hidden. \nerr c0rяup data\n" << endl;
-		}
-		else if (con_input == "help dir")
-		{
-			cout << "Displays a list of files and subdirectories in a directory. \n dir [options] \n\n /a Show all files, including hidden. \nerr c0rяup data\n" << endl;
 		}
 		else if (con_input == "e x i t")
 		{
